03f6c3108dd8c3e34880ccb8aa46ae35354ac85b
Kathy <kathyl200517@gmail.com>
2019-11-02 17:59:05 -0700

    add validation

--- ch_2_assign_kathy_li.py
+++ ch_2_assign_kathy_li.py
@@ -12,50 +12,56 @@ while True:
 			continue
 		elif delete_team in teams.keys():
 			teams.pop(delete_team)
-			print("Team " + str(delete_team) + " has been removed. Returning to menu... ")
+			print("Team " + str(delete_team) + " has been removed.  ")
 			continue
 		else: 
-			print("Team " + str(delete_team) + " has not been found. Returning to menu... ")
+			print("Team " + str(delete_team) + " has not been found.  ")
 			continue
 	
 	elif user_action == "modify": 
-		update_team = int(input("Which team would you like to modify? To return the to the menu, enter 0. \n "))
-		if update_team == 0:
-			continue
-		elif update_team in teams.keys():
-			update_variable = input("Which field would you like to modify: name, programming language, width, length, number of drivetrain motors, or camera vision? \n")
-			if update_variable not in teams[new_team].keys():
-				print("There is no such field. Returning to menu... ")
+		update_team = input("Which team would you like to modify? To return the to the menu, enter 0. \n")
+		if update_team.isnumeric(): 
+			update_team = int(update_team)		
+			if update_team == 0:
 				continue
-			change = input("Enter the new value. \n")
-			if update_variable in ["width", "length", "number of drivetrain motors"]: 
-				if not change.isnumeric():
-					print("Input is invalid. Returning to menu... ")
+			elif update_team in teams.keys():
+				update_variable = input("Which field would you like to modify: name, programming language, width, length, number of drivetrain motors, or camera vision? \n")
+				if update_variable not in teams[new_team].keys():
+					print("There is no such field. ")
 					continue
+				change = input("Enter the new value. \n")
+				if update_variable in ["width", "length", "number of drivetrain motors"]: 
+					if not change.isnumeric():
+						print("Input is invalid.  ")
+						continue
+					else:
+						teams[update_team][update_variable] = change
+						print("Successfully changed.   ")
+						continue
+				elif update_variable == "camera vision":
+					if change != "True" and change != "False":
+						print("Please input True or False.  ")
+						continue
+					else: 
+						change = bool(change)
+						teams[update_team][update_variable] = change
+						print("Successfully changed.  ")
+						continue
 				else:
 					teams[update_team][update_variable] = change
-					print("Successfully changed. Returning to menu... ")
+					print("Successfully changed.  ")
 					continue
-			elif update_variable == "camera vision":
-				change = bool(change)
-				if not change == True or change == False:
-					print("Please input True or False. Returning to menu... ")
-					continue
-				else: 
-					teams[update_team][update_variable] = change
-					print("Successfully changed. Returning to menu... ")
 			else:
-				teams[update_team][update_variable] = change
-				print("Successfully changed. Returning to menu... ")
+				print("Team " + str(update_team) + " was not found.  ")
 				continue
 		else:
-			print("Team " + str(update_team) + " was not found. Returning to menu... ")
+			print("Team " + str(update_team) + " was not found.  ")
 			continue
 
 	elif user_action == "add": 
 		new_team = input("Enter the number of the team you want to add. To return to the menu, enter 0. \n")
 		if not new_team.isnumeric():
-			print("Must be a number. Returning to menu... ")
+			print("Must be a number.  ")
 			continue
 		elif new_team == "0":
 			continue
@@ -66,32 +72,32 @@ while True:
 			teams[new_team]["programming language"] = input("What programming language does the team use? \n")
 			width = input("What is the width of the team's robot? \n")
 			if not width.isnumeric():
-				print("Width must be a number. Returning to menu... ")
+				print("Width must be a number.  ")
 				continue
 			else: 
 				width = float(width)
 				teams[new_team]["width"] = width
 			length = input("What is the length of the team's robot? \n")
 			if not length.isnumeric():
-				print("Length must be a number. Returning to menu... ")
+				print("Length must be a number.  ")
 				continue
 			else: 
 				length = float(width)
 				teams[new_team]["length"] = length
 			camera_vision = input("Does the team have a camera vision system? Input True or False. \n")
-			camera_vision = bool(camera_vision)
-			if not camera_vision == True or camera_vision == False:
-				print("Please follow directions. Returning to menu... ")
+			if camera_vision != "True" and camera_vision != "False":
+				print("Please follow directions.")
 				continue
 			else:
+				camera_vision = bool(camera_vision)
 				teams[new_team]["camera vision"] = camera_vision
 			drivetrain_motors = input("How many drivetrain motors does the robot have? \n")
 			if not drivetrain_motors.isnumeric(): 
-				print("Drivetrain motors must be a number. Returning to menu... ")
+				print("Drivetrain motors must be a number.  ")
 				continue
 			else:
 				teams[new_team]["number of drivetrain motors"] = drivetrain_motors
-			print("Team added. Returning to menu... ")
+			print("Team added.  ")
 			continue
 	
 	elif user_action == "view": 
@@ -108,16 +114,21 @@ while True:
 			print(teams[view_input])
 			continue
 		else: 
-			print("Team " + view_input + " has not been found. Returning to menu... ")
+			print("Team " + str(view_input) + " has not been found.  ")
 			continue
 	elif user_action == "search": 
-		search_input = int(input("Which team would you like to search for? To return to the menu, enter 0. \n"))
-		if search_input == 0:
-			continue
-		elif search_input in teams.keys():
-			print(teams[search_input])
+		search_input = input("Which team would you like to search for? To return to the menu, enter 0. \n")
+		if search_input.isnumeric():
+			search_input = int(search_input)
+			if search_input == 0:
+				continue
+			elif search_input in teams.keys():
+				print(teams[search_input])
+			else:
+				print("Team does not exist.  ")
+				continue
 		else:
-			print("Team does not exist. Returning to menu... ")
+			print("Invalid team. ")
 			continue
 	elif user_action == "quit":
 		break
