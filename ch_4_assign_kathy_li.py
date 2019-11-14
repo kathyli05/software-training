899e716d8c9aa622d445a010f37227d4293203f6
Kathy <kathyl200517@gmail.com>
2019-11-13 19:15:54 -0800

    Change 3D point variables to "x", "y", and "z".
    Add super() function.

--- ch_4_assign_kathy_li.py
+++ ch_4_assign_kathy_li.py
@@ -6,16 +6,15 @@ class Point:
         self.y = y
     
     def distance(self):
-       return math.sqrt(x**2 + y**2)
+       return math.sqrt(self.x**2 + self.y**2)
 
 class Point_3D(Point):
-    def __init__ (self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def __init__ (self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
 
     def distance(self):
-        return math.sqrt(a**2 + b**2 + c**2)
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
 while True:
     x = input("What is the x-value of your point? \n" )
@@ -36,27 +35,27 @@ while True:
     point_a = Point(x, y) 
     print("The distance from the origin is " + str(point_a.distance()))
 
-    a = input("What is the x-value of your point? \n" )
-    if a.isnumeric():
-        a = int(a)
+    x = input("What is the x-value of your point? \n" )
+    if x.isnumeric():
+        x = int(x)
     else:
         print("Invalid input.")
         continue
 
-    b = input("what is the y-value of your point? \n")
-    if b.isnumeric():
-        b = int(b)
+    y = input("what is the y-value of your point? \n")
+    if y.isnumeric():
+        y = int(y)
     else:
         print("Invalid input.")
         continue
 
-    c = input("What is the z-value of your point? \n")
-    if c.isnumeric():
-        c = int(c)
+    z = input("What is the z-value of your point? \n")
+    if z.isnumeric():
+        z = int(z)
     else:
         print("Invalid input.")
         continue
-    point_b = Point_3D(a, b, c)
+    point_b = Point_3D(x, y, z)
     
     print("The distance from the origin is " + str(point_b.distance()))
 
@@ -66,3 +65,4 @@ while True:
         print("The 3D point is further from the origin than the 2D point.")
     elif point_a.distance() == point_b.distance():
         print("The points are the same distance from the origin.")
+    break
